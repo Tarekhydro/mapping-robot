1493aa70018ea58a6989fa85e9ef833cd6e9e88e
Tarekhydro <50757358+Tarekhydro@users.noreply.github.com>
2020-10-05 23:09:18 -0400

    color

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-    <div style={{ overflowY: 'scroll', height: '500px', border: '3px solid black' }}>
+    <div style={{ overflowY: 'scroll', height: '500px', border: '5px solid black' }}>
         {props.children}
     </div>
     )
